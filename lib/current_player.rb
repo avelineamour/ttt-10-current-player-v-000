63589af3374db6a359b3e6b112d13e032c808f9e
Razelle McCarrick <razelle@satyaorthopedic.com>
2017-11-28 00:47:41 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-counter = 0
+
 def turn_count(board)
+  counter = 0
   board.each
-  if board[index] == "X" || board[index] == "O"
+  if board[] == "X" || board[index] == "O"
     counter += 1
     puts "#{counter}"
   end
