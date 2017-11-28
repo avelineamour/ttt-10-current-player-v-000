35e5ecc5c73ad74c571eedd3d0f5e0751d71b82c
Razelle McCarrick <razelle@satyaorthopedic.com>
2017-11-28 00:49:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,8 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
   counter = 0
-  board.each
-    counter += 1
-    puts "#{counter}"
+  board.each{counter += 1}
+  puts "#{counter}"
 end
 
 def current_player
